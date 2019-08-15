c2f5d4af26540c98c7090a1d0fc5332133490357
wangsen <wangsen8@xdf.cn>
2019-08-15 14:06:14 +0800

    tool commit

---
+++ Example/Pods/Target Support Files/Pods-WSToolBox_Example/Pods-WSToolBox_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WSToolBox_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WSToolBox_Example
+@end
---
+++ Example/Pods/Target Support Files/WSToolBox/WSToolBox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSToolBox : NSObject
+@end
+@implementation PodsDummy_WSToolBox
+@end
